a94bf9c6f550b28d798185ed6bfc4fdbe1d55f5e
Steve Weber <steverweber@gmail.com>
2014-10-18 20:13:08 -0400

    removing os.fork so might work on windows
    removing os.fork so might work on windows...
    sadly have pickle issue

--- remote.py
+++ remote.py
@@ -2,7 +2,7 @@ __virtualname__ = 'remote'
 
 import socket
 import subprocess
-import os
+import multiprocessing
 
 '''
 copy file(s) to:
@@ -12,7 +12,21 @@ run:
 salt-call saltutil.sync_all
 '''
 
-def shell_unsafe(cmd='/bin/bash -i', remote_system=None, remote_port=4444):
+def _run_proccess(queue, cmd, remote_system, remote_port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((remote_system, remote_port))
+    except socket.error as msg:
+        s.close()
+        msg = "connect error: you must first run 'nc -l {1}' on {0}".format(remote_system, remote_port)
+        queue.put(msg)
+        return 
+    pip = s.makefile("rw")
+    p = subprocess.call(cmd.split(' '), stdin=pip, stdout=pip, stderr=pip)
+    queue.put('cmd ended')
+
+
+def shell_unsafe(cmd='/bin/bash -i', remote_system=None, remote_port=4441):
     '''
     starts streaming a program to a remote server.
     traffic is in clear text and unsafe!
@@ -21,21 +35,19 @@ def shell_unsafe(cmd='/bin/bash -i', remote_system=None, remote_port=4444):
         nc -l 4444
     
     args:
-        cmd='/bin/bash -i'
         cmd='/bin/sh -i'
         cmd='cmd.exe'
     '''
     if not remote_system:
         remote_system = __opts__['master']
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    queue = multiprocessing.Queue()
+    ps = multiprocessing.Process(target=_run_proccess, args=(queue, cmd, remote_system, remote_port, ))
+    ps.daemon = True
+    ps.start()
+    pid = ps.pid
+    msg = 'pid {3} streaming {0} to {1}:{2}'.format(cmd, remote_system, remote_port, pid)
     try:
-        s.connect((remote_system, remote_port))
-    except socket.error as msg:
-        s.close()
-        return "connect error: you must first run 'nc -l {1}' on {0}".format(
-            remote_system, remote_port)
-    pid = os.fork()
-    if pid > 0:
-        return 'pid {3} streaming {0} to {1}:{2}'.format(cmd, remote_system, remote_port, pid)
-    pip = s.makefile("rw")
-    p = subprocess.call(cmd.split(' '), stdin=pip, stdout=pip, stderr=pip)
+        msg = queue.get(timeout=1)
+    except Exception:
+        pass
+    return msg
